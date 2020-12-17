e379d6b5ee01947470a07b6f5c84be5d0d479354
LtTodra <lttodta@lttodra.de>
2020-12-17 16:56:05 +0100

    Basics

---
+++ src/main/java/de/lttodra/ttt/main/Main.java
@@ -0,0 +1,17 @@
+package de.lttodra.ttt.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
